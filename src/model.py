60fb98388a1b5d1ac6ee8de19529f29ad15d47b0
Bhausaheb Markande <markandebhausaheb@gmail.com>
2026-06-15 15:43:45 +0530

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 00:11:30 2026
+
+@author: dell
+"""
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+def build_lstm_model(time_step, n_features=1):
+    model = Sequential()
+    model.add(LSTM(64, return_sequences=True, input_shape=(time_step, n_features)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(64))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    return model
---
+++ src/preprocessing.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 00:11:57 2026
+
+@author: dell
+"""
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+def create_sequences(data, time_step):
+    X, y = [], []
+    for i in range(len(data) - time_step):
+        X.append(data[i:i+time_step])
+        y.append(data[i+time_step])
+    return np.array(X), np.array(y)
+
+def scale_data(values):
+    scaler = MinMaxScaler()
+    scaled = scaler.fit_transform(values.reshape(-1,1))
+    return scaled, scaler
